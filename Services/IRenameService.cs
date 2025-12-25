b8ab5cc7066deb877dff1dec5a12fd6e06ba9c58
lsj1206 <1007ckddjs12@gmail.com>
2025-12-26 00:27:32 +0900

    Comment: Add

--- Services/IRenameService.cs
+++ Services/IRenameService.cs
@@ -10,14 +10,14 @@ namespace TagNamer.Services;
 public interface IRenameService
 {
     /// <summary>
-    /// 규칙을 적용하여 모든 아이템의 미리보기 이름(NewName)을 업데이트합니다.
+    /// 규칙을 적용하여 모든 아이템의 미리보기 이름을 업데이트합니다.
     /// </summary>
     void UpdatePreview(IEnumerable<FileItem> items, string ruleFormat, TagManagerViewModel tagManager, bool showExtension);
 
     /// <summary>
     /// 미리보기 상태의 이름을 실제 파일 시스템에 적용합니다.
     /// </summary>
-    List<FileItem> ApplyRename(IEnumerable<FileItem> items, bool showExtension);
+    void ApplyRename(IEnumerable<FileItem> items, bool showExtension);
 
     /// <summary>
     /// 변경된 이름을 이전 상태로 되돌립니다.
